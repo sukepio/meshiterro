d0471c2ef238004e475730571bc1b0cb06417fb1
sukepio <naoto927sukena@gmail.com>
2020-12-13 00:31:33 +0000

    [Start] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd429deb0b8dde6b86b77707c1b45d3ad032cd47478440d341210c0dd4db4e7cb771c1eecede8ef059fe3c6b8f8b1b08d3f69fa7b0680dc71c07058672be70fa8'
